d445e16ecaa5447240557302114ae2922969063f
Luffy <bestparty2025@gmail.com>
2025-06-27 03:44:58 +0800

    feat: add IsTest and IsProd functions to profile package for improved profile management

--- profile/profile.go
+++ profile/profile.go
@@ -9,26 +9,26 @@ const (
 	ProfileProd = "prod"
 )
 
-// IsDev checks if the current profile is development.
-// Returns true if the service is running in development mode.
 func IsDev() bool {
 	return IsDevStr(_profile)
 }
 
-// IsDevStr checks if the given profile string is the development profile.
-// Returns true if the profile string matches the development profile.
+func IsTest() bool {
+	return IsTestStr(_profile)
+}
+
+func IsProd() bool {
+	return IsProdStr(_profile)
+}
+
 func IsDevStr(profile string) bool {
 	return strings.ToLower(profile) == ProfileDev
 }
 
-// IsTestStr checks if the given profile string is the test profile.
-// Returns true if the profile string matches the test profile.
 func IsTestStr(profile string) bool {
 	return strings.ToLower(profile) == ProfileTest
 }
 
-// IsProdStr checks if the given profile string is the production profile.
-// Returns true if the profile string matches the production profile.
 func IsProdStr(profile string) bool {
 	return strings.ToLower(profile) == ProfileProd
 }
